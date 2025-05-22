18933b55cc8541434b67887ed055c8e4dc3f555d
Parul-Locobuzz <parul.kondekar@locobuzz.com>
2025-05-23 00:48:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'about-me-app';
+  name = 'Parul Kondekar';
+  bio = 'im learning angular because my code needed more plot twists and who needs sleep anyway:)';
+  profilePic = 'https://media.licdn.com/dms/image/v2/D5612AQGqPGHORIsa-g/article-cover_image-shrink_720_1280/article-cover_image-shrink_720_1280/0/1691871730624?e=2147483647&v=beta&t=HT4mUN4oFnzQAJ7wz1X7pDqRp38vifBP8d9SkyG7hW4'; 
+  pic='https://www.shutterstock.com/image-vector/this-cat-pixel-art-cute-260nw-2183120025.jpg';
 }
